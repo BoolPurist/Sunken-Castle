b4867690cc45bed2e7b7af5ae0c7a31a951473f7
Florian <naumannfl79974@th-nuernberg.de>
2020-05-14 06:01:53 +0200

    Added Click Event for New Game
    Created a new GameObject "MenueEvents" as a container for  scripts for a menue level respectively. Added the child "EventsMainMenue" of  "MenueEvents" with the click function "StartNewGame" to the button New Game. Now when clicked scene changes to a certain scene. Scene is assigned by giving the name of it as parameter to the click function. Created scene "Level".

---
+++ Rogue-Game-2D-Unity-Project/Assets/Scripts/Menue/StartNewGame.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartNewGame : MonoBehaviour
+{
+    public void OnClickStartNewGame(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
