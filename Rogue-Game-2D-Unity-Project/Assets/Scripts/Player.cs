16b693752d47602707dca95510ec527273d6779e
BauerPa78971 <bauerpa78971@th-nuernberg.de>
2020-05-15 11:48:01 +0200

    Written movement in the Player Skript
    - Created new scene for testing
    - Added a few textures for testing
    - Created Player script, added the movement in all directions with arrow-keys | wasd | controller

---
+++ Rogue-Game-2D-Unity-Project/Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    public Animator animator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
+
