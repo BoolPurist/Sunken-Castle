7dadc153a4973229cd1f3e883a9880d80bf0a75a
BauerPa78971 <bauerpa78971@th-nuernberg.de>
2020-05-17 19:53:17 +0200

    Reworked Enemy and Player Script
    -Deleted MovingObjects abstract Class FOR NOW as it was causing trouble
    - Reworked Enemyscript to follow the player
    - gave enemy needed attributes such as: maxHealth, currentHealth, isAlive
    - set currentHealth attribute to private for Player and Enemy and added a get-Property
    - Added an enemy Tag for Objects
    - Added a test enemy sprite in textures

---
+++ Rogue-Game-2D-Unity-Project/Assets/Scripts/MoveableObject/Enemy.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform player;
+    public Rigidbody2D rb;
+    public float moveSpeed = 2f;
+    private Vector2 movement;
+
+    public int MaxHealth = 100;
+    private int currentHealth = 100;
+    public bool isAlive = true;
+
+    public int CurrentHealth
+    {
+        get { return currentHealth; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+        direction.Normalize(); //Brings the value of "direction" between "-1" and "1" for the movement function
+        movement = direction;
+    }
+
+    private void FixedUpdate() //
+    {
+        moveCharacter(movement);
+    }
+
+    void moveCharacter(Vector2 direction)
+    {
+        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+}
--- Rogue-Game-2D-Unity-Project/Assets/Scripts/MoveableObject/Player.cs
+++ Rogue-Game-2D-Unity-Project/Assets/Scripts/MoveableObject/Player.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
+    public int maxHealth = 100;
+    private int currentHealth = 100;
+    public bool isAlive = true;
+    
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
     Vector2 movement;
     public Animator animator;
-    public int maxHealth = 100;
-    public int currentHealth = 100;
-    public bool isAlive = true;
 
-    // Update is called once per frame
+    public int CurrentHealth
+    {
+        get { return currentHealth; }
+    }
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
