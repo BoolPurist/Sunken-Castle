a78f707383d42abe5beb6aa246dba087b1a6b72a
BauerPa78971 <bauerpa78971@th-nuernberg.de>
2020-05-19 18:28:43 +0200

    Player attack in 4 directions
    1. Added attack in all 4 directions for the player via the arrow keys
    2. arrow keys can still be used for walking, this will be fixed
    3. planning on parting the "player" script into "playerInfo" and "playerMovement" for better overview and a cooler UML diagramm at a later time
    4. Some small changes to the test scene

--- Rogue-Game-2D-Unity-Project/Assets/Scripts/MoveableObject/PlayerAttack.cs
+++ Rogue-Game-2D-Unity-Project/Assets/Scripts/MoveableObject/PlayerAttack.cs
@@ -8,18 +8,54 @@ public class PlayerAttack : MonoBehaviour
     public float timeBtwAttacks; //Time the player has to wait before attacking again, public so changeable by the designer (in seconds, so value 5f means 5 seconds)
     private float curTimeBtwAttacks; //Time until the next attack can be started
 
-    public Transform attackPos; //Position of the attack
+    
+    public Transform attackPosRight; //Position of the attack
+    public Transform attackPosLeft;
+    public Transform attackPosUp;
+    public Transform attackPosDown; 
+
+
     public float attackRange; //Range of the attack
-    public LayerMask whatIsEnemy;
-    public int damage;
+    public LayerMask whatIsEnemy; 
+    public int damage; //Damage dealt to enemies in radius
 
     void Update()
     {
         if(curTimeBtwAttacks <= 0) //If the minimal time between two attacks has passed, you can attack
         {
-            if (Input.GetKey(KeyCode.Space))
+            if (Input.GetKey(KeyCode.RightArrow))
             {
-                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemy);
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPosRight.position, attackRange, whatIsEnemy);
+                //Circle gets cast at certain position, all enemies in the circle get put into array enemiesToDamage, which will take damage
+
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+                }
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPosLeft.position, attackRange, whatIsEnemy);
+                //Circle gets cast at certain position, all enemies in the circle get put into array enemiesToDamage, which will take damage
+
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+                }
+            }
+            else if (Input.GetKey(KeyCode.UpArrow))
+            {
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPosUp.position, attackRange, whatIsEnemy);
+                //Circle gets cast at certain position, all enemies in the circle get put into array enemiesToDamage, which will take damage
+
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+                }
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPosDown.position, attackRange, whatIsEnemy);
                 //Circle gets cast at certain position, all enemies in the circle get put into array enemiesToDamage, which will take damage
 
                 for (int i = 0; i < enemiesToDamage.Length; i++)
@@ -38,7 +74,7 @@ public class PlayerAttack : MonoBehaviour
     void OnDrawGizmosSelected() //Visualizes the attack for testing
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+        Gizmos.DrawWireSphere(attackPosRight.position, attackRange);
 
     }
 }
