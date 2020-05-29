c077ce4ed4b956bbfd59a2d9438a5c9ec4874a5f
zieglerja80870 <64924252+zieglerja80870@users.noreply.github.com>
2020-05-29 15:25:25 +0100

    Implemented Functionability for Menu and Quit Buttons

---
+++ Rogue-Game-2D-Unity-Project/Assets/Scripts/Menue/PauseResume.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Game is paused when ESC get pressed
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    // Game is resumed
+    public void Resume()
+    {
+        // Scene before Pause_Menu_Scene is loaded
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    // Game is paused
+    void Pause()
+    {
+        SceneManager.LoadScene(Pause_Menu_Scene); //
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
