0498496dda06b44e306f92877739d74bb263b01b
zieglerja80870 <64924252+zieglerja80870@users.noreply.github.com>
2020-05-30 12:40:58 +0100

    Added PauseMenu
    - Resume Button (also working with ESC)
    - Menu Button
    - Quit Button

---
+++ Rogue-Game-2D-Unity-Project/Assets/Scripts/Menue/PauseMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Game is paused when ESC get pressed
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    // Game is resumed
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    // Game is paused
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+}
