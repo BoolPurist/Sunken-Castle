8b9243ce768c374d96f9415737c6f5f8c48cca1e
wintron04 <barthel_a@web.de>
2020-06-05 18:27:17 +0200

    Testlevel
    -Further work on TestLevel
    -Added preview of Enemy_Attack animation
    -Added Healing Bottle Light
    -Created alpha Tilemap
    -Changed Enemy_Attack script in order to allow Enemy_Attack animation
    -Changed Pick Up Info script in order to destroy Light when bottle is "picked up"
    -Added Portal Light

--- Rogue-Game-2D-Unity-Project/Assets/Scripts/MoveableObject/EnemyAttack.cs
+++ Rogue-Game-2D-Unity-Project/Assets/Scripts/MoveableObject/EnemyAttack.cs
@@ -9,20 +9,28 @@ public class EnemyAttack : MonoBehaviour
     public int damage = 10;
     public Vector2 attackRange;
     private int angle = 0;
+    private Animator anim;
 
     public float timeBtwEnemyAttack;
     private float curTimeBtwEnemyAttack;
 
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
     void Update()
     {
         if (curTimeBtwEnemyAttack <= 0)
         {
             Collider2D[] Player = Physics2D.OverlapBoxAll(attackPos.position, attackRange, angle, whatIsPlayer);
+
             for (int i = 0; i < Player.Length; i++)
             {
                 Player[i].GetComponent<PlayerInfo>().TakeDamage(damage);
                 if (Player[i].GetComponent<PlayerInfo>().CurrentHealth <= 0)
                     Destroy(gameObject);
+                anim.SetTrigger("Attacking");
             }
 
 
@@ -31,8 +39,9 @@ public class EnemyAttack : MonoBehaviour
         else
         {
             curTimeBtwEnemyAttack -= Time.deltaTime;
+            anim.ResetTrigger("Attacking");
         }
-        
+
     }
 
     void OnDrawGizmosSelected() //Visualizes the attack for testing
