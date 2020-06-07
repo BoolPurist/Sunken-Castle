d5fc56240f077f6766f853b88ddbc7988554ef20
wintron04 <barthel_a@web.de>
2020-06-07 19:59:07 +0200

    Refined Testlevel
    -Added Casual & Lianen Tree animation in Test scene
    In Scene:
    - Added Tombstones
    - Added Roots
    - Added Puddle
    - Added Gras
    - Added Statue
    
    - Added Sorting Layer script (Sorting Layers are now set through script which needs to be a component of every GameObject in Scene)

---
+++ Rogue-Game-2D-Unity-Project/Assets/Scripts/PositionRendererSorter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PositionRendererSorter : MonoBehaviour
+{
+    private Renderer myRender;
+    [SerializeField]
+    private int sortingOrderBase = 5000;
+    [SerializeField]
+    private int offset = 0;
+    [SerializeField]
+    private bool updateOnce = false;
+
+
+
+
+    void Awake()
+    {
+        myRender = GetComponent<Renderer>();
+
+    }
+
+    void LateUpdate()
+    {
+        myRender.sortingOrder = (int)(sortingOrderBase - (transform.position.y * 100) - offset);
+
+        if(updateOnce)
+            Destroy(this);
+    }
+
+
+    
+}
