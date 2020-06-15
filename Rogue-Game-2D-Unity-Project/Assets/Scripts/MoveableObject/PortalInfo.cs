f631b83e46df44f12f05c2afee933b2fb071a520
BauerPa78971 <bauerpa78971@th-nuernberg.de>
2020-06-15 12:26:06 +0200

    Started work on level change
    Created portalInfo Script which checks if enemies are left in the level, if not, it activates(no function yet) and then checks if a player touches the portal, if yes, it changes Levels(function not implemented yet

---
+++ Rogue-Game-2D-Unity-Project/Assets/Scripts/MoveableObject/PortalInfo.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalInfo : MonoBehaviour
+{
+    public Transform portal;
+    public LayerMask WhatIsEnemy;
+    public LayerMask WhatIsPlayer;
+    public Vector2 levelRadius;
+    public Vector2 portalRadius;
+
+    private bool isActivated = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isActivated == false)
+        {
+            Collider2D[] Enemy = Physics2D.OverlapBoxAll(portal.position, levelRadius, WhatIsEnemy);
+            if(Enemy[0] == null)
+            {
+                Activate();
+            }
+        }
+        else
+        {
+            Collider2D[] Player = Physics2D.OverlapBoxAll(portal.position, portalRadius, WhatIsPlayer);
+            if(Player[0] != null)
+            {
+                ChangeLevel();
+            }
+        }
+    }
+
+    void Activate()
+    {
+        isActivated = true;
+    }
+
+    void ChangeLevel()
+    {
+
+    }
+
+    void OnDrawGizmosSelected() //Visualizes the levelRadius and portalRadius for testing
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(portal.position, (Vector3)levelRadius);
+        Gizmos.DrawWireCube(portal.position, (Vector3)portalRadius);
+    }
+}
