b77783aa2384f723c7ce42c78feaa693d3b37903
Florian <naumannfl79974@th-nuernberg.de>
2020-06-25 14:40:49 +0200

    Created script for scene wide object and prefab for object handling random scene choice
    - The script "ManageSceneWideObject" has a method to create a gameobject with tag from prefab. It onlys creates one if the scene wide object has not been created so far.
    - This script also has method to delete the scene wide object by searching for its tag. If the gameobject is not there nothing happens.
    - Created prefab that will handle choosing a random scene.

---
+++ Rogue-Game-2D-Unity-Project/Assets/Scripts/Menue/ManageSceneWideObject.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManageSceneWideObject : MonoBehaviour
+{
+    public GameObject prefabSceneWideObject;
+
+    private string prefabSceneWideObjectTag;
+
+    private void Awake()
+    {
+        this.prefabSceneWideObjectTag = prefabSceneWideObject.tag;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SpawnSceneWideObject()
+    {
+        if (GameObject.FindGameObjectWithTag(this.prefabSceneWideObjectTag) == null)
+        {
+            GameObject sceneWideObject = Instantiate(this.prefabSceneWideObject);
+
+            if (this.prefabSceneWideObjectTag == "Untagged")
+            {
+                Debug.LogError("The prefab for the scene wide object is not tagged !");
+                return;
+            }
+
+            this.prefabSceneWideObjectTag = sceneWideObject.tag;
+
+            // This causes the object to continue to exits throughout the scene.
+            DontDestroyOnLoad(sceneWideObject);
+        }
+    }
+
+    public void DeleteSceneWideObject()
+    {
+        GameObject sceneWideObject = GameObject.FindGameObjectWithTag(prefabSceneWideObjectTag);
+
+        if (sceneWideObject != null)
+        {
+            Destroy(sceneWideObject);
+        }
+    }
+}
