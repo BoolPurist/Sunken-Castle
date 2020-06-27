3f62821791bcaf02610f164609cf52fbc518d23d
Florian <naumannfl79974@th-nuernberg.de>
2020-06-27 16:54:35 +0200

    Script "ChooseRandomScene" now inherents from Script "ManageSceneWideObject"
    - Also added some tooltips for unity inpectors in the Script "ChooseRandomScene" and "ManageSceneWideObject".

--- Rogue-Game-2D-Unity-Project/Assets/Scripts/Menue/ManageSceneWideObject.cs
+++ Rogue-Game-2D-Unity-Project/Assets/Scripts/Menue/ManageSceneWideObject.cs
@@ -3,17 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class ManageSceneWideObject : MonoBehaviour
 {
     // Prefab for object which exits through serveral scenes.
+    [Tooltip("Prefab as base for the object to exit over serverls scenes.")]
     public GameObject prefabSceneWideObject;
 
-    private string prefabSceneWideObjectTag;
+    protected string prefabSceneWideObjectTag;
 
-    private void Awake()
+    protected void Awake()
     {
         // Storing tag assigned to the prefab for later checking if the object already exits.
-        this.prefabSceneWideObjectTag = prefabSceneWideObject.tag;
+        this.prefabSceneWideObjectTag = this.prefabSceneWideObject.tag;
     }
 
     // Start is called before the first frame update
