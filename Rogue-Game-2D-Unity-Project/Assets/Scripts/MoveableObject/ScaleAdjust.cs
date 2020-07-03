7bd9ea63ee5c93a9ce8910a42c5fd159d11d8255
wintron04 <barthel_a@web.de>
2020-07-03 18:44:26 +0200

    Added ScaleAdjust for Enemies ONLY

---
+++ Rogue-Game-2D-Unity-Project/Assets/Scripts/MoveableObject/ScaleAdjust.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleAdjust : MonoBehaviour
+{
+    private Transform transform;
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+        transform.localScale = new Vector3(3f, 3f, 2.1869f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Destroy(this);
+    }
+}
